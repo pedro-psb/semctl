4606da3bf76e794f3ec7edac62e2f4ecac51eb56
Pedro Brochado <pedropsb95@gmail.com>
2025-12-03 20:40:28 -0300

    Implentacao estrutural de semctl e bloco de controle

--- src/bloco_controle.vhd
+++ src/bloco_controle.vhd
@@ -38,9 +38,147 @@ library IEEE;
 
 
 entity bloco_controle is
+  port (
+    -- I/O externo
+    clk, rst : in  std_logic;
+    in_mad, in_car1, in_car2 : in  std_logic;
+    out_fsm : out  std_logic_vector(9 downto 0);
+    sem1 : out std_logic_vector(1 downto 0);
+    sem2 : out std_logic_vector(1 downto 0);
+    ped1 : out std_logic_vector(1 downto 0);
+    ped2 : out std_logic_vector(1 downto 0);
+    ped3 : out std_logic_vector(1 downto 0);
+
+    -- I/O interno
+    count_done  : in std_logic;
+    car1_enable : out std_logic;
+    car2_enable : out std_logic;
+    polaridade_out  : out std_logic
+  );
 end entity;
 
 
 architecture structural of bloco_controle is
+  type fsm_estados is (
+    INITIAL, MADR,
+    ALL_CLOSED,
+    PRE_SEM1_OPEN, PRE_SEM2_OPEN,
+    SEM1_OPEN,     SEM2_OPEN,
+    POS_SEM1_OPEN, POS_SEM2_OPEN
+  );
+  signal PS, NS : fsm_estados;
+  signal polaridade: std_logic;
 begin
+  polaridade_out <= polaridade;
+  -- Externalizar estados
+  with PS select
+        out_fsm <=
+            "0000000001" when POS_SEM2_OPEN,
+            "0000000010" when SEM2_OPEN,
+            "0000000100" when PRE_SEM2_OPEN,
+            "0000001000" when ALL_CLOSED,
+            "0000010000" when PRE_SEM1_OPEN,
+            "0000100000" when SEM1_OPEN,
+            "0001000000" when POS_SEM1_OPEN,
+
+            "1000000000" when INITIAL,
+            "0100000000" when MADR,
+            "1111111111" when others
+    ;
+
+  -- Atualiza estado FSM
+  fsm_sync: process(CLK, RST)
+  begin
+    if RST = '1' then
+      PS <= INITIAL;
+    elsif (rising_edge(CLK)) then
+      PS <= NS;
+    end if;
+  end process fsm_sync;
+
+
+  fsm_comb: process(PS, in_mad) is
+    constant PISC : STD_LOGIC_VECTOR(1 downto 0) := "00";
+    constant VERM : STD_LOGIC_VECTOR(1 downto 0) := "01";
+    constant AMAR : STD_LOGIC_VECTOR(1 downto 0) := "10";
+    constant VERD : STD_LOGIC_VECTOR(1 downto 0) := "11";
+  begin
+    case PS is
+      -- especiais
+      when INITIAL =>
+        if in_mad = '1' then
+          NS <= MADR;
+        elsif count_done = '1' then
+          NS <= ALL_CLOSED;
+        end if;
+        sem1 <= PISC; ped1 <= PISC;
+        sem2 <= PISC; ped2 <= PISC;
+        ped3 <= PISC;
+        polaridade <= '1';
+      when MADR =>
+        if in_mad = '1' then
+          NS <= MADR;
+        elsif count_done = '1' then
+          NS <= ALL_CLOSED;
+        end if;
+        sem1 <= PISC; ped1 <= PISC;
+        sem2 <= PISC; ped2 <= PISC;
+        ped3 <= PISC;
+      when ALL_CLOSED =>
+        if in_mad = '1' then
+          NS <= MADR;
+        elsif count_done = '1' and polaridade = '0' then
+          NS <= PRE_SEM1_OPEN;
+        elsif count_done ='1' and polaridade = '1' then
+          NS <= PRE_SEM2_OPEN;
+        else NS <= INITIAL;
+        end if;
+        sem1 <= VERM; ped1 <= VERD;
+        sem2 <= VERM; ped2 <= VERD;
+        ped3 <= VERD;
+
+      -- ciclo direita
+      when PRE_SEM2_OPEN =>
+        polaridade <= not polaridade;  -- inverte polaridade do ciclo
+        if count_done = '1' then NS <= SEM2_OPEN; end if;
+        sem1 <= VERM; ped1 <= VERD;
+        sem2 <= VERM; ped2 <= AMAR;
+        ped3 <= AMAR;
+      when SEM2_OPEN =>
+        if count_done = '1' then NS <= POS_SEM2_OPEN; end if;
+        sem1 <= VERM; ped1 <= VERD;
+        sem2 <= VERD; ped2 <= VERM;
+        ped3 <= VERM;
+      when POS_SEM2_OPEN =>
+        if count_done = '1' then NS <= ALL_CLOSED; end if;
+        sem1 <= VERM; ped1 <= VERD;
+        sem2 <= AMAR; ped2 <= VERM;
+        ped3 <= VERM;
+
+      -- ciclo esquerda
+      when PRE_SEM1_OPEN =>
+        polaridade <= not polaridade;  -- inverte polaridade do ciclo
+        if count_done = '1' then NS <= SEM1_OPEN; end if;
+        sem1 <= VERM; ped1 <= AMAR;
+        sem2 <= VERM; ped2 <= VERD;
+        ped3 <= AMAR;
+      when SEM1_OPEN =>
+        if count_done = '1' then NS <= POS_SEM1_OPEN; end if;
+        sem1 <= VERD; ped1 <= VERM;
+        sem2 <= VERM; ped2 <= VERD;
+        ped3 <= VERM;
+      when POS_SEM1_OPEN =>
+        if count_done = '1' then NS <= ALL_CLOSED; end if;
+        sem1 <= AMAR; ped1 <= VERM;
+        sem2 <= VERM; ped2 <= VERD;
+        ped3 <= VERM;
+
+      -- Others
+      when others =>
+        if count_done = '1' then NS <= INITIAL; end if;
+        sem1 <= PISC; ped1 <= PISC;
+        sem2 <= PISC; ped2 <= PISC;
+        ped3 <= PISC;
+    end case;
+  end process fsm_comb;
 end architecture;
--- src/semctl.vhd
+++ src/semctl.vhd
@@ -1,4 +1,23 @@
 -- Controle de Semáforo - Componente principal
+--
+--
+-- input:
+--   clk: 1 bit
+--  rst: 1 bit       - Reseta para estado inicial
+--   in_car1: 1 bits  - Enable para sensor de carro 1
+--   in_car2: 1 bits  - Enable para sensor de carro 2
+--   in_mad:  1 bits  - Ativa modo madrugada
+-- output:
+--   sem1: 2 bits     - Saida para semaforo de carros 1
+--   sem2: 2 bits     - Saida para semaforo de carros 2
+--   ped1: 2 bits     - Saida para semaforo de pedrestres 1
+--   ped2: 2 bits     - Saida para semaforo de pedrestres 2
+--   ped3: 2 bits     - Saida para semaforo de pedrestres 3
+--
+-- Integrantes:
+-- * Guilherme Augusto
+-- * Pedro Armando
+-- * Pedro Pessoa
 
 library IEEE;
   use IEEE.STD_LOGIC_1164.ALL;
@@ -23,118 +42,47 @@ end entity;
 
 
 architecture structural of semctl is
-  type fsm_estados is (
-    INITIAL, MADR,
-    ALL_CLOSED,
-    PRE_SEM1_OPEN, PRE_SEM2_OPEN,
-    SEM1_OPEN,     SEM2_OPEN,
-    POS_SEM1_OPEN, POS_SEM2_OPEN
-  );
-  signal PS, NS : fsm_estados;
-  signal X : STD_LOGIC;  -- sinaliza mudança de estado
-  signal polaridade: std_logic; -- sinaliza ciclo. 0/esquerda, 1/direita
+  -- componentes
+  component bloco_controle is
+    port (
+      clk, rst : in  std_logic;
+      in_mad, in_car1, in_car2 : in  std_logic;
+      out_fsm : out  std_logic_vector(9 downto 0);
+      sem1 : out std_logic_vector(1 downto 0);
+      sem2 : out std_logic_vector(1 downto 0);
+      ped1 : out std_logic_vector(1 downto 0);
+      ped2 : out std_logic_vector(1 downto 0);
+      ped3 : out std_logic_vector(1 downto 0);
+
+      count_done  : in std_logic;
+      car1_enable : out std_logic;
+      car2_enable : out std_logic;
+      polaridade_out  : out std_logic
+    );
+  end component;
+
+  -- conexoes inter-componente
+  signal count_done: std_logic := '1';
+  signal car1_enable: std_logic;
+  signal car2_enable: std_logic;
+  signal polaridade_out: std_logic;
 begin
-  -- Externalizar estados
-  with PS select
-        out_fsm <=
-            "0000000001" when POS_SEM2_OPEN,
-            "0000000010" when SEM2_OPEN,
-            "0000000100" when PRE_SEM2_OPEN,
-            "0000001000" when ALL_CLOSED,
-            "0000010000" when PRE_SEM1_OPEN,
-            "0000100000" when SEM1_OPEN,
-            "0001000000" when POS_SEM1_OPEN,
-
-            "1000000000" when INITIAL,
-            "0100000000" when MADR,
-            "1111111111" when others
-    ;
-
-  -- Atualiza estado FSM
-  fsm_sync: process(CLK, RST)
-  begin
-    if RST = '1' then
-      PS <= INITIAL;
-    elsif (rising_edge(CLK)) then
-      PS <= NS;
-    end if;
-  end process fsm_sync;
-
-
-  fsm_comb: process(PS) is
-    constant PISC : STD_LOGIC_VECTOR(1 downto 0) := "00";
-    constant VERM : STD_LOGIC_VECTOR(1 downto 0) := "01";
-    constant AMAR : STD_LOGIC_VECTOR(1 downto 0) := "10";
-    constant VERD : STD_LOGIC_VECTOR(1 downto 0) := "11";
-  begin
-    case PS is
-      -- especiais
-      when INITIAL =>
-        NS <= ALL_CLOSED;
-        sem1 <= PISC; ped1 <= PISC;
-        sem2 <= PISC; ped2 <= PISC;
-        ped3 <= PISC;
-        -- X <= '0';
-        X <= '1';  -- so para efeitos de teste
-        polaridade <= '1';
-      when MADR =>
-        if X = '1' then NS <= ALL_CLOSED; end if;
-        sem1 <= PISC; ped1 <= PISC;
-        sem2 <= PISC; ped2 <= PISC;
-        ped3 <= PISC;
-      when ALL_CLOSED =>
-        if X = '1' then
-          if polaridade = '0' then NS <= PRE_SEM1_OPEN;
-          elsif polaridade = '1' then NS <= PRE_SEM2_OPEN;
-          else NS <= INITIAL;
-          end if;
-        end if;
-        sem1 <= VERM; ped1 <= VERD;
-        sem2 <= VERM; ped2 <= VERD;
-        ped3 <= VERD;
-
-      -- ciclo direita
-      when PRE_SEM2_OPEN =>
-        polaridade <= not polaridade;  -- inverte
-        if X = '1' then NS <= SEM2_OPEN; end if;
-        sem1 <= VERM; ped1 <= VERD;
-        sem2 <= VERM; ped2 <= AMAR;
-        ped3 <= AMAR;
-      when SEM2_OPEN =>
-        if X = '1' then NS <= POS_SEM2_OPEN; end if;
-        sem1 <= VERM; ped1 <= VERD;
-        sem2 <= VERD; ped2 <= VERM;
-        ped3 <= VERM;
-      when POS_SEM2_OPEN =>
-        if X = '1' then NS <= ALL_CLOSED; end if;
-        sem1 <= VERM; ped1 <= VERD;
-        sem2 <= AMAR; ped2 <= VERM;
-        ped3 <= VERM;
-
-      -- ciclo esquerda
-      when PRE_SEM1_OPEN =>
-        polaridade <= not polaridade;  -- inverte
-        if X = '1' then NS <= SEM1_OPEN; end if;
-        sem1 <= VERM; ped1 <= AMAR;
-        sem2 <= VERM; ped2 <= VERD;
-        ped3 <= AMAR;
-      when SEM1_OPEN =>
-        if X = '1' then NS <= POS_SEM1_OPEN; end if;
-        sem1 <= VERD; ped1 <= VERM;
-        sem2 <= VERM; ped2 <= VERD;
-        ped3 <= VERM;
-      when POS_SEM1_OPEN =>
-        if X = '1' then NS <= ALL_CLOSED; end if;
-        sem1 <= AMAR; ped1 <= VERM;
-        sem2 <= VERM; ped2 <= VERD;
-        ped3 <= VERM;
-
-      -- Others
-      when others =>
-        if X = '1' then NS <= INITIAL; end if;
-        sem1 <= PISC; ped1 <= PISC;
-        sem2 <= PISC; ped2 <= PISC;
-        ped3 <= PISC;
-    end case;
-  end process fsm_comb;
+  datapath : bloco_controle
+  port map(
+    clk => clk,
+    rst => rst,
+    in_mad => in_mad,
+    in_car1 => in_car1,
+    in_car2 => in_car2,
+    out_fsm => out_fsm,
+    sem1 => sem1,
+    sem2 => sem2,
+    ped1 => ped1,
+    ped2 => ped2,
+    ped3 => ped3,
+    count_done => count_done,
+    car1_enable => car1_enable,
+    car2_enable => car2_enable,
+    polaridade_out => polaridade_out
+  );
 end architecture;

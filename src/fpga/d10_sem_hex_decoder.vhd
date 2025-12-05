b517e291cbc03b466590ead13d5e4da6ce338867
Pedro Brochado <pedropsb95@gmail.com>
2025-12-05 13:54:01 -0300

    Update sem_hex_decoder
    Update sem_hex decoder de novo

--- src/fpga/d10_sem_hex_decoder.vhd
+++ src/fpga/d10_sem_hex_decoder.vhd
@@ -1,18 +1,13 @@
--- Decoder estado do sinal para codigo do display de 7 segmentos (D10)
+-- Decoder estado do sinal para codigo do display de 7 segmentosi (D10)
 --
 -- Os semaforos de carro (2) e de pedestre (3) serão representados por displays de
 -- 7 segmentos da seguinte forma:
 --
--- Este decoder agora aceita DOIS estados de semáforo e produz DOIS displays separados:
--- sem_state_up: estado do semáforo superior -> display_config_up
--- sem_state_down: estado do semáforo inferior -> display_config_down
---
--- |   _    ||     |     |  _  |     | 00 - piscando
--- |  |_|   || | | ||    |     |   | | 01 - fechado
--- |  |_|   || | | |   | |  _  | |   | 10 - amarelo
--- | -------||-----|-----|-----|-----| 11 - aberto
--- | est_up || 00  | 01  | 10  | 11  |
--- | est_dw || 00  | 11  | 10  | 11  |
+-- |   _    ||  _  |  _  |     |     | 0 - piscando
+-- |  |_|   || |_| |     |  _  |     | 1 - fechado
+-- |  |_|   || |   |     |     |  _  | 2 - amarelo
+-- | -------||-----|-----|-----|-----| 3 - aberto
+-- | estado || 00  | 01  | 10  | 11  |
 --
 
 library IEEE;
@@ -20,35 +15,21 @@ library IEEE;
   use IEEE.NUMERIC_STD.ALL;
 
 entity d10_sem_hex_decoder is port (
-  sem_state_up: in std_logic_vector(1 downto 0);
-  sem_state_down: in std_logic_vector(1 downto 0);
-  display_config_up: out std_logic_vector(6 downto 0);
-  display_config_down: out std_logic_vector(6 downto 0)
+  sem_in: in std_logic_vector(1 downto 0);
+  hex_out: out std_logic_vector(6 downto 0)
   );
 end entity;
 
 
 architecture dataflow of d10_sem_hex_decoder is
 begin
-  -- Decode upper state to upper display
-  display_config_up <=
-    -- O vetor corresponde aos sinais (6 5 4 3 2 1 0) do display 7-segmentos
-    -- Mapping: 0=top, 1=top-right, 2=bottom-right, 3=bottom, 4=bottom-left, 5=top-left, 6=middle
-    -- O pauzinho acende no valor '0'! (active low)
-    not "1111110" when sem_state_up = "00" else  -- 00 - piscando: top segment only
-    not "1000001" when sem_state_up = "01" else  -- 01 - fechado: all except top and middle
-    not "0111111" when sem_state_up = "10" else  -- 10 - amarelo: middle segment only
-    not "0000000" when sem_state_up = "11" else  -- 11 - aberto: all segments
-    not "1111111";  -- Default case (all off)
-
-  -- Decode lower state to lower display
-  display_config_down <=
-    -- O vetor corresponde aos sinais (6 5 4 3 2 1 0) do display 7-segmentos
-    -- Mapping: 0=top, 1=top-right, 2=bottom-right, 3=bottom, 4=bottom-left, 5=top-left, 6=middle
-    -- O pauzinho acende no valor '0'! (active low)
-    not "1111110" when sem_state_down = "00" else  -- 00 - piscando: top segment only
-    not "1000001" when sem_state_down = "01" else  -- 01 - fechado: all except top and middle
-    not "0111111" when sem_state_down = "10" else  -- 10 - amarelo: middle segment only
-    not "0000000" when sem_state_down = "11" else  -- 11 - aberto: all segments
-    not "1111111";  -- Default case (all off)
+  hex_out <=
+    -- O vetor corresponde aos sinais (6 5 ... 0) do display 7-segmentos
+    -- Os numeros 0..6 correspondem a pauzinhos especificos do display (ver documentacao)
+    -- O pauzinho acende no valor '0'!
+    not "1110011" when sem_in = "00" else
+    not "0000001" when sem_in = "01" else
+    not "1000000" when sem_in = "10" else
+    not "0001000" when sem_in = "11" else
+    not "0000000";
 end architecture;
